fe70aa7623acca08fe9e25cbe4b8748d109f15c2
NadzeyaHope <radkovanada13@gmail.com>
2024-03-06 11:52:11 +0300

    display data from back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,95 @@
-export default function Home() {
+interface GetProductsParams {
+	offset: number;
+	limit: number;
+}
+
+const getProducts = async (params: GetProductsParams) => {
+	const { offset, limit } = params;
+
+	const response = await fetch('https://api.valantis.store:41000/', {
+		method: 'POST',
+		headers: {
+			'X-Auth': '5c8431a5765a73a9394ab0169a2a96b6',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			action: 'get_ids',
+			params: { offset, limit }
+		})
+	});
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	}
+
+	const { result: ids } = await response.json();
+	// todo: remove duplicates
+
+	{
+		const response = await fetch('https://api.valantis.store:41000/', {
+			method: 'POST',
+			headers: {
+				'X-Auth': '5c8431a5765a73a9394ab0169a2a96b6',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				action: 'get_items',
+				params: { ids }
+			})
+		});
+
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+
+		const { result: products } = await response.json();
+
+		return products;
+	}
+};
+
+
+const Home = async () => {
+	const products = await getProducts({
+		offset: 0,
+		limit: 50,
+	});
+
+	console.log(products);
+
+
 	return (
 		<main>
+			<h1>Product List</h1>
+			{products.map((product: any) => (
+				<>
+					<table key={product.id}>
+						<tbody>
+						<tr>
+							<th>Id</th>
+							<td>{product.id}</td>
+						</tr>
+						<tr>
+							<th>Name</th>
+							<td>{product.product}</td>
+						</tr>
+						<tr>
+							<th>Brand</th>
+							<td>{product.brand}</td>
+						</tr>
+						<tr>
+							<th>Price</th>
+							<td>{product.price}</td>
+						</tr>
+						</tbody>
+					</table>
+					<br/>
+					<br/>
+				</>
+			))}
 
 		</main>
 	);
-}
+};
+
+export default Home;
